a6281477f6d12b2f7f4acb9a2713748c39c2c3a2
Gowra Sivannarayana <sivannarayana210@gmail.com>
2025-04-25 15:19:24 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <!-- Wrap components in Suspense to handle loading -->
+    <Suspense>
+      <template #default>
+        <!-- Wrap all Components inside a single root element -->
+        <div>
+          <!-- Sticky NavBar at the top -->
+          <div class="sticky top-0 left-0 right-0 z-10 bg-[#111827]">
+            <NavBar />
+          </div>
+          <div class="pt-[80px]">
+            <!-- Other sections will follow after the Navbar -->
+            <HeroSection />
+            <ServicesSection />
+            <AboutSection />
+            <ExperienceAndSkills />
+            <LatestProjSection />
+            <ContactSection />
+            <Footer />
+            <BackToTop />
+          </div>
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <loadingSpinner />
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup>
+  import { defineAsyncComponent } from 'vue';
+  // Import Components Asynchronously
+  const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+  const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'));
+  const ServicesSection = defineAsyncComponent(() => import('@/components/ServicesSection.vue'));
+  const AboutSection = defineAsyncComponent(() => import('@/components/AboutSection.vue'));
+  const TestimonialsSection = defineAsyncComponent(() => import('@/components/TestimonialsSection.vue'));
+  const LatestProjSection = defineAsyncComponent(() => import('@/components/LatestProjSection.vue'));
+  const ExperienceAndSkills = defineAsyncComponent(() => import('@/components/ExperienceAndSkills.vue'));
+  const ContactSection = defineAsyncComponent(() => import('@/components/ContactSection.vue'));
+  const Footer = defineAsyncComponent(() => import('@/components/Footer.vue'));
+  const BackToTop = defineAsyncComponent(() => import('@/components/BackToTop.vue'));
+  import loadingSpinner from './components/loadingSpinner.vue';
+</script>
+
+<style>
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: #111827 #f1f1f1;
+  }
+
+  /* Sticky NavBar */
+  .sticky {
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 20;
+    background-color: #ed7409;
+    box-shadow: 0 2px 4px rgba(234, 228, 228, 0.1);
+  
+    
+  }
+
+  /* Add padding to the body content below the navbar */
+  .pt-[80px] {
+    padding-top: 80px; /* Adjust this based on your NavBar height */
+  }
+</style>
---
+++ src/components/ExperienceAndSkills.vue
@@ -0,0 +1,103 @@
+<template>
+    <section class="text-white mt-18" id="skills">
+        <div class="absolute right-0 top-[110rem] h-full w-full justify-end">
+            <span class="flex opacity-20">
+                <span class="w-16 h-32 rounded-l-full flex bg-primary blur-2xl"></span>
+                <span class="w-16 h-32 rounded-r-full flex bg-[#f88fc2] blur-2xl mt-14"></span>
+            </span>
+        </div>
+        <div class="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 xl:px-16">
+            <div class="mt-4 md:mt-0 text-left flex flex-col z-10 h-full w-[80%]">
+                <h2 class="text-4xl font-bold text-white text-left mb-4">My 
+                    <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">Skills</span>
+                </h2>
+                <div class="mt-8" v-for="skill in Skills" :key="skill.id">
+                    <div class="flex items-end justify-between" data-aos="fade-right">
+                        <h4 class="font-semibold uppercase text-white">
+                            {{ skill.name }}
+                        </h4>
+                        <h3 class="text-xl font-bold text-white">{{ skill.width }}</h3>
+                    </div>
+                    <div class="mt-2 h-1 w-full bg-[#131d30] rounded-full">
+                        <div class="h-1 rounded-full bg-primary" :style="`width :${skill.width}`"></div>
+                    </div>
+                </div>
+            </div>
+            <div data-aos="flip-left">
+                <h2 class="text-4xl font-bold text-white text-left mb-8 md:text-center md:mt-0 mt-8">My Experiences</h2>
+                <div class="space-y-8 py-4" data-aos="fade-left">
+                    <div v-for="element in Experinces" :key="element.id"
+                    class="flex  items-center rounded-xl p-4 bg-[#111a3e] shadow-lg border border-[#1f1641]"
+                    >
+                    <div class="w-1/4">
+                        <img src="https://img.icons8.com/ios-filled/100/ffffff/lawyer.png" alt="lawyer">
+                    </div>
+                    <div class="w-3/4 pl-4">
+                        <h3 class="text-xl font-semibold uppercase text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
+                            {{ element.role }}
+                        </h3>
+                        <p class="text-white">{{ element.company }}</p>
+                        <p class="text-white">{{ element.date }}</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+import { ref } from 'vue';
+const Skills =ref([
+    {
+        id:1,
+        name:'HTML5 & CSS3',
+        width:'85%'
+    },
+    {
+        id:2,
+        name:'Tailwind CSS &  Bootstrap',
+        width:'75%'
+    },
+    {
+        id:3,
+        name:'JavaScript',
+        width:'98%'
+    },
+    {
+        id:4,
+        name:'Python',
+        width:'70%'
+    },
+    {
+        id:5,
+        name:'vue.js & TypeScript',
+        width:'60%'
+    },
+    {
+        id:6,
+        name:'git, github & bitbucket',
+        width:'80%'
+    },
+]);
+
+const Experinces =ref([
+    {
+        id:1,
+        role:' UI Developer',
+        company:'FINSE Technologies Pvt Ltd.',
+        date:'Apr 2024 - Feb 2025'
+    },
+    {
+        id:2,
+        role:' Software Development Engineer Intern',
+        company:'FINSE Technologies Pvt Ltd.',
+        date:'Oct 2023 - Mar 2024'
+    },
+    {
+        id:3,
+        role:' CISCO Networking Organization – Internship',
+        company:'CISCO',
+        date:'Dec 2018 – May 2019'
+    },
+])
+</script>
